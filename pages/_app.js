13972f4b97b830f6c5041a4aef7c3d76f3b0ab6a
DaillanLeigh <francodaillanleigh@gmail.com>
2023-05-03 20:47:44 +0800

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Layout from "@/components/Layouts/layout";
+import { Poppins } from "next/font/google";
+import KrustyProvider from "@/components/ContextProvider/KrustyContextProvider";
+const poppins = Poppins({ weight: "400", subsets: ["devanagari"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <KrustyProvider>
+      <main className={poppins.className}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
+    </KrustyProvider>
+  );
 }
